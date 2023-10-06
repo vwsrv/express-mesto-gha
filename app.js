5f47c5cccc9c617c2daf509f2d93f8a06a07a386
Bl Bltich <vissar2016@yandex.ru>
2023-10-07 01:27:16 +0300

    fix: eslint code fix

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 import usersRouter from './routes/users';
 import cardsRouter from './routes/cards';
 
-const { PORT = 3000,
-  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb'
+const {
+  PORT = 3000,
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(json());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '651ddc563928613a987fef63'
+    _id: '651ddc563928613a987fef63',
   };
   next();
 });
@@ -23,9 +24,9 @@ app.use(cardsRouter);
 
 async function initServer() {
   await mongoose.connect(MONGO_URL);
-  console.log('DataBase is connected...')
-  await app.listen(PORT)
-  console.log('Server successfully started!')
+  console.log('DataBase is connected...');
+  await app.listen(PORT);
+  console.log('Server successfully started!');
 }
 
 initServer();
--- validations/card.js
+++ validations/card.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 const cardsValidation = (data) => {
   const cardsSchema = Joi.object({
-    name: Joi.string().min(2).max(40).required(),
-    link: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required(),
   });
   return cardsSchema.validate(data);
 };
